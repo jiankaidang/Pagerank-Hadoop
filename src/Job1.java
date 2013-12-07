597ae4041970f794c128378548beb11f90d4c7f3
jiankaidang <jiankai.dang@nyu.edu>
2013-12-06 20:55:37 -0500

    Job1

---
+++ src/Job1.java
@@ -0,0 +1,69 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Author: Jiankai Dang
+ * Date: 12/6/13
+ */
+public class Job1 {
+    public static void main(String[] args) throws Exception {
+        Job job = new Job(new Configuration());
+        job.setJarByClass(Job1.class);
+
+        job.setNumReduceTasks(10);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setMapperClass(Map.class);
+        job.setCombinerClass(Combine.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(KeyValueTextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path("Job2/loop0/"));
+
+        job.waitForCompletion(true);
+    }
+
+    public static class Map extends Mapper<Text, Text, Text, Text> {
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+            context.write(key, new Text(value.toString().replace("_", " ")));
+        }
+    }
+
+    private static class Combine extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            StringBuilder builder = new StringBuilder();
+            for (Text val : values) {
+                builder.append(val);
+            }
+            context.write(key, new Text(builder.toString()));
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            StringBuilder builder = new StringBuilder();
+            builder.append(1).append("\t");
+            for (Text val : values) {
+                builder.append(val);
+            }
+            context.write(key, new Text(builder.toString()));
+        }
+    }
+}
---
+++ src/Job3.java
@@ -0,0 +1,6 @@
+/**
+ * Author: Jiankai Dang
+ * Date: 12/6/13
+ */
+public class Job3 {
+}
