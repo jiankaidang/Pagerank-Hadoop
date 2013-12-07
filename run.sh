d959d5d0b0fd87f09dd78bb384cbef06924b7a16
jiankaidang <jiankai.dang@nyu.edu>
2013-12-07 00:01:36 -0500

    Job3

--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@ input_filename=$2
 output_directory=$3
 
 #RUN THE FIRST JOB
-hadoop jar hadoop-streaming.jar Job1 $input_directory/$input_filename
+hadoop jar hadoop-pagerank.jar Job1 $input_directory/$input_filename
 
 #RUN THE SECOND JOB
 for ((i=1; i<=$N; i++))
 do
         #Make sure that the output of ith iteration is the input of (i+1)th iteration
-        hadoop jar hadoop-streaming.jar Job2 $i
+        hadoop jar hadoop-pagerank.jar Job2 $i
 
         #Remove the output of previous iterations here. we only need output of the most recent iteration.
         rm -rf Job2/loop$(($i - 1))
--- src/Job3.java
+++ src/Job3.java
@@ -1,7 +1,69 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.util.TreeMap;
+
 /**
  * Author: Jiankai Dang
  * Date: 12/6/13
  */
 
 public class Job3 {
+    public static void main(String[] args) throws Exception {
+        Job job = new Job(new Configuration());
+        job.setJarByClass(Job3.class);
+
+        job.setNumReduceTasks(1);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(KeyValueTextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path("Job2/loop10/part-r-00000"));
+        FileOutputFormat.setOutputPath(job, new Path(args[0] + "/top100"));
+
+        job.waitForCompletion(true);
+    }
+
+    public static class Map extends Mapper<Text, Text, Text, Text> {
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+            String[] node = value.toString().split("\t");
+            context.write(key, new Text(node[0]));
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, Text, Text, Text> {
+        TreeMap<Double, String> priorityQueue = new TreeMap<Double, String>();
+
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            priorityQueue.put(Double.parseDouble(values.iterator().next().toString()), key.toString());
+            if (priorityQueue.size() > 100) {
+                priorityQueue.pollFirstEntry();
+            }
+        }
+
+        protected void cleanup(Context context)
+                throws IOException,
+                InterruptedException {
+            while (!priorityQueue.isEmpty()) {
+                java.util.Map.Entry<Double, String> entry = priorityQueue.pollLastEntry();
+                context.write(new Text(entry.getValue()), new Text(entry.getKey().toString()));
+            }
+        }
+    }
 }
