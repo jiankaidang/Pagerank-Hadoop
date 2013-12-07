d521e48999c24492640775bb7c2d995a03488106
jiankaidang <jiankai.dang@nyu.edu>
2013-12-06 22:58:04 -0500

    rm -rf Job2/loop$(($i - 1))

--- run.sh
+++ run.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
+
 #There are 3 Hadoop jobs to compute the pagerank
+
 N=10 #termination condition
+
 input_directory=$1
 input_filename=$2
 output_directory=$3
+
 #RUN THE FIRST JOB
 hadoop jar hadoop-streaming.jar Job1 $input_directory/$input_filename
+
+#RUN THE SECOND JOB
+for ((i=1; i<=$N; i++))
+do
+        #Make sure that the output of ith iteration is the input of (i+1)th iteration
+        hadoop jar hadoop-streaming.jar Job2 $i
+
+        #Remove the output of previous iterations here. we only need output of the most recent iteration.
+        rm -rf Job2/loop$(($i - 1))
+
+        echo DONE ITERATION $i
+done
